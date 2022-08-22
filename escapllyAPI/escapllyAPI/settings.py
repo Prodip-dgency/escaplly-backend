1fe5e7cb22ce981628a2937d65a2c3436be01d34
Prodip <p.k.sorkar007@gmail.com>
2022-08-22 16:51:26 +0600

    collected all the staticfiles

--- escapllyAPI/escapllyAPI/settings.py
+++ escapllyAPI/escapllyAPI/settings.py
@@ -141,6 +141,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Media files (user uploaded content e.g. images, videos etc.)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
