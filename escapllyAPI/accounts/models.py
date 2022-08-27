1b82f8cf985a0203506a9f4ee1c1e854f10ed4f3
Prodip <p.k.sorkar007@gmail.com>
2022-08-27 18:00:02 +0600

    Configuring custom user model and user profile

--- escapllyAPI/accounts/models.py
+++ escapllyAPI/accounts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 # Create your models here.
+class MyUser(AbstractUser):
+    pass
+    username = models.CharField(_('username'), max_length=50, null=True, blank=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class UserProfile(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500, null=True, blank=True)
+    address = models.CharField(max_length=500, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+
+
+
+    
--- escapllyAPI/escapllyAPI/settings.py
+++ escapllyAPI/escapllyAPI/settings.py
@@ -70,6 +70,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'escapllyAPI.urls'
 
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -152,3 +154,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.MyUser'
